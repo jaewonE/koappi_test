e3bf0d88c58b4e9bb2b135c8b22ec35df632841f
jaewonE <importjaewonE@gmail.com>
2021-09-17 10:04:15 +0900

    create copy & kakao share btn on landingPage

--- src/router/landing/SubmitLandingPage.js
+++ src/router/landing/SubmitLandingPage.js
@@ -6,23 +6,37 @@ const SubmitLandingPage = ({ setSubmitDone }) => {
 
   const submitLanding = async (e) => {
     e.preventDefault();
-    // const { target } = e;
-    // const name = target[0].value;
-    // const email = target[1].value;
-    // const message = target[2].value;
-    // const landingObj = {
-    //   name,
-    //   email,
-    //   message,
-    //   getInterview: isChecked,
-    // };
-    // const error = await addLandingMessage(landingObj);
-    // if (error) {
-    //   console.error(error);
-    // } else {
-    //   setSubmitDone(true);
-    // }
-    setSubmitDone(true);
+    const { target } = e;
+    const name = target[0].value;
+    const email = target[1].value;
+    const message = target[2].value;
+    const landingObj = {
+      name,
+      email,
+      message,
+      getInterview: isChecked,
+    };
+    //위 과정은 유저가 입력한 정보를 가져와 landingObj 라는 object형태로 만든 것.
+
+    const error = await addLandingMessage(landingObj);
+    //해당 object를 firebase firestore에 저장한다.
+    //firebase.fireStore이 뭔지 모르겠으면 내일 전화줘.
+    //addLandingMessage는 내가 만든 함수로 components/firebase/fIndex 경로에 있어.
+    //addLandingMessage 함수는 landing 컬렉션 안에 messages 문서 안에 있는 리스트에 위 landingObj를 추가하는 함수야
+    //그렇다고 landing 컬렉션과 messages문서를 생성할 필요는 없어. 없을 경우 생성하는 기능이 두었어.
+    //에러 객체를 리턴하는데 에러가 없을 경우 null값을 return해. 그렇기에 아래와 같이 if문을 통해 error값이 있을 경우에 대해 상황을 설정 할 수 있어.
+    //return되는 형태는 promise이기 때문에 (위 코드와 같이) 사용할 때 반드시 await을 앞에 입력해주어야 되!
+
+    if (error) {
+      console.error(error); //에러가 있을 경우 에러를 출력하고
+    } else {
+      setSubmitDone(true);
+      // 에러가 없으면 setSubmitDone(true) 함수를 실행시켜 submitDone 변수의 값을 true로 변경한다.
+      //landing.js를 보면 삼항연산자로 submitDone 변수의 값이 true일 경우 landingSuccess 컴포넌트를, 그렇지 않을 경우 landingInfo 컴포넌트를 보여주게 되어있는데
+      //위의 addLandingMessage함수를 통해 firebase server에 유저의 정보를 저장하는 과정에 에러가 없을 경우 실행되는 setSubmitDone(true)를 통해
+      //submitDone의 값이 false에서 true로 변경되었기 때문에 landingSuccess 컴포넌트가 유저에게 보여진다.
+      //(submitDone의 초기값은 false이다. submitDone 변수를 선언할 때 useState(false)를 통해 초기값을 false로 주었기 때문.)
+    }
   };
   return (
     <React.Fragment>
--- src/router/landing/selectLangPage.js
+++ src/router/landing/selectLangPage.js
@@ -1,7 +1,102 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import logo from 'images/landing/logo.png';
 
+const useShare = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
+    script.async = true;
+    document.head.appendChild(script);
+    return () => {
+      if (script) {
+        document.head.removeChild(script);
+      }
+    };
+  }, []);
+  function copyToClipboard(val) {
+    const t = document.createElement('textarea');
+    document.body.appendChild(t);
+    t.value = val;
+    t.select();
+    document.execCommand('copy');
+    document.body.removeChild(t);
+  }
+  function copy(text) {
+    copyToClipboard(String(text));
+    alert('copy');
+  }
+  const clipboardShare = () => {
+    console.log('clipboardShare');
+    navigator.permissions
+      .query({ name: 'clipboard-write' })
+      .then((permissions) => {
+        if (permissions !== 'granted') {
+          copy('https://tripu.fun/#/landing');
+        } else {
+          const copy_url = String('https://tripu.fun/#/landing');
+          navigator.clipboard.writeText(copy_url).then(
+            () => {
+              alert('copy');
+            },
+            () => {
+              alert('error');
+            }
+          );
+        }
+      });
+  };
+  const kakaoShare = () => {
+    console.log('kakaoShare');
+    const copy_url = 'https://tripu.fun/#/landing';
+    try {
+      if (window.Kakao) {
+        const kakao = window.Kakao;
+        if (!kakao.isInitialized()) {
+          kakao.init('c17e02c875b0ed1852e6c348cd7da81a');
+        }
+        kakao.Link.sendDefault({
+          objectType: 'feed',
+          content: {
+            title: 'TripU : customized Korean travel platform',
+            description:
+              'we are preparing hard now! Cheer for our one dream here',
+            imageUrl: 'https://tripu.fun/static/media/logo.d34f1a49.png', //이거 이미지 주소로 수정
+            link: {
+              mobileWebUrl: copy_url,
+              webUrl: copy_url,
+              androidExecutionParams: copy_url,
+              iosExecutionParams: copy_url,
+            },
+          },
+          social: {
+            likeCount: 254,
+            sharedCount: 333,
+            viewCount: 513,
+          },
+          buttons: [
+            {
+              title: 'Move to Homepage',
+              link: {
+                mobileWebUrl: copy_url,
+                webUrl: copy_url,
+                androidExecutionParams: copy_url,
+                iosExecutionParams: copy_url,
+              },
+            },
+          ],
+        });
+      }
+    } catch (error) {
+      clipboardShare();
+    }
+  };
+  return { clipboardShare, kakaoShare };
+};
+
 const SelectLangPage = ({ setLanguage }) => {
+  const { clipboardShare, kakaoShare } = useShare();
+
   const submitLang = (e) => {
     e.preventDefault();
     const {
@@ -33,6 +128,33 @@ const SelectLangPage = ({ setLanguage }) => {
             value="中國語"
           />
         </div>
+        {/* 여기서 부터 */}
+        <div className="landingShare-wrapper">
+          <label
+            htmlFor="landing-share_copy"
+            className="social-icon social-icon--copy"
+          >
+            <i className="fas fa-copy"></i>
+            <input
+              type="button"
+              id="landing-share_copy"
+              onClick={clipboardShare}
+            />
+            <div className="name">copy</div>
+          </label>
+          <div className="social-icon social-icon--kakaoTalk">
+            <label htmlFor="landing-share_kakao">
+              <i className="fas fa-comment"></i>
+            </label>
+            <input
+              type="button"
+              id="landing-share_kakao"
+              onClick={kakaoShare}
+            />
+            <div className="name">Kakao Talk</div>
+          </div>
+        </div>
+        {/* 여기까지 */}
       </div>
     </div>
   );
